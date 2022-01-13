dcdcddb4bcb411ae0e138d7f9fe7eb43885a808c
dmitryduy <cdcvh14@gmail.com>
2022-01-13 15:39:05 +0300

    fix

--- src/beautifyPhone.ts
+++ src/beautifyPhone.ts
@@ -1,6 +1,6 @@
-import { phone } from "../backend/types";
 
 
-export const beautifyPhone = (phone: phone): phone=>  {
+
+export const beautifyPhone = (phone: string): string=>  {
     return `+ ${phone[0]} ${phone.slice(1, 4)} ${phone.slice(4, 7)} ${phone.slice(7)}`;
 }
